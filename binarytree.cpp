9747050ee0fd5fb5377a557bb599428f01bd4367
aldys002 <igidiaaldys@gmail.com>
2024-06-13 15:29:55 +0700

    membuat anak kiri dari poin ortu untuk node baru

--- binarytree.cpp
+++ binarytree.cpp
@@ -33,6 +33,22 @@ public :
       Node* newNode = new Node(element, nullptr, nullptr); // allocate memory for the new node
       newNode->info = element; // Assign value to the data field of the new node
       newNode->leftchild = nullptr; // make the left child of the new node point to null
+      newNode->rightchild = nullptr; // make the right child of the new node point  to null
+
+      Node* parent = nullptr;
+      Node* currentNode = nullptr;
+      search(element, parent, currentNode); //locate the node which will be the parent of the node to be 
+
+      if (parent == nullptr) // if the parent is NULL (tree is empty)
+      {
+        ROOT = newNode; // Mark the new node as root
+        return; // Exit
+      }
+
+      if (element < parent->info) // if the value in the data field of the new node is less than of the
+      {
+        parent->leftchild = newNode; // Make the left child of the parent point to the new node
+      }
    }
 
 };
