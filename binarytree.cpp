f1601c3c8c8ea0c9902aaca269dedc4725a5317d
aldys002 <igidiaaldys@gmail.com>
2024-06-13 15:09:13 +0700

    membuat class binary tree

--- binarytree.cpp
+++ binarytree.cpp
@@ -12,6 +12,13 @@ public :
     //Constructor for the node class
     Node(string i, Node* l, Node* r)
     {
-        
+        info = i;
+        leftchild = l;
+        rightchild = r;
     }
 };
+
+class BinaryTree 
+{
+
+}
